86899e4a0e2861d7c83aadf5670795fe57690fbd
Ivan <ivanka322@gmail>
2023-02-02 13:42:15 +0300

    Решение второй задачи

---
+++ HomeWork02/Program.cs
@@ -0,0 +1,27 @@
+﻿// // Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// // 645 -> 5
+
+// // 78 -> третьей цифры нет
+
+// // 32679 -> 6
+// Console.clear();
+Console.Clear();
+
+Console.Write("ВВедите число : ");
+int number = int.Parse(Console.ReadLine()!);
+
+while (number > 999)
+{
+    number = number / 10;
+}
+
+int result = number % 10;
+if (number < 99)
+{
+    Console.WriteLine($"Третьего номера нет"!);
+}
+else
+{
+    Console.WriteLine($"{result}"!);
+}
